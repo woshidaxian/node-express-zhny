b7e65ac9f0226b867d2dec90c5e1ed3a7b0d8c92
woshidaxian <huang15057081701@163.com>
2021-02-05 20:23:55 +0800

    1

--- routes/otherAPI.js
+++ routes/otherAPI.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 // 获取天气
+// 调用getWeather.py
+// 将python返回的结果返回给前端
 router.get('/weather',function(req,res,next){
     
 })
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// 管理员登录操作
+router.post('/login', function(req,res,next){
+  let acc = req.body.account;
+  let pass = req.body.password;
+})
+
 module.exports = router;
